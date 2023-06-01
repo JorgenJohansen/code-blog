aac4bf2650d3975ba361133e2178055a006516f9
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2023-06-01 15:42:12 +0200

    Added blogheader

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,13 +1,42 @@
 const { default: PageLayout } = require("@/components/PageLayout")
 import { useRouter } from "next/router";
 import { getBlogBySlug, getAllBlogs } from "@/lib/api";
+import { Row, Col } from 'react-bootstrap'
+import BlogHeader from 'components/BlogHeader';
+
+import BlockContent from '@sanity/block-content-to-react';
+
+const serializers = {
+    types: {
+        code: () => {
+            return <h1>Code Block will be here</h1>
+        }
+    }
+}
 
 const BlogDetail = ({blog}) => {
     const {query} = useRouter();
     return (
-        <PageLayout>
-            <h1>Hello Detail Page - {blog?.slug}</h1>
-        </PageLayout>
+        <PageLayout className="blog-detail-page">
+            <Row>
+                <Col md={{ span: 10, offset: 1 }}>
+                <BlogHeader
+                    title={blog.title}
+                    subtitle={blog.subtitle}
+                    coverImage={blog.coverImage}
+                    author={blog.author}
+                    date={blog.date}
+                />
+                <hr/>
+                {/* Blog Content Here */}
+                <BlockContent 
+                imageOptions={{w: 320, h: 240, fit: 'max'}}
+                serializers={serializers}
+                blocks={blog.content}
+                />
+                </Col>
+            </Row>
+    </PageLayout>
     )
 }
 
